d830791a152baeb6ed54b75a6a3f5fc7143016db
Hareesh Gangadhara <hareesh.b@tatadigital.com>
2023-07-08 12:44:38 +0530

    Added new ViewController

---
+++ GitBasicOperationsDemo/ThirdViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ThirdViewController.swift
+//  GitBasicOperationsDemo
+//
+//  Created by Hareesh Gangadhara on 08/07/23.
+//  Copyright © 2023 Hareesh Gangadhara. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("My first statement for Git Demo..")
+        print("one line added for pull command check")
+        print("My second statement for Git")
+        
+        print("second line added for pull command check")
+        print("Added new line for commit")    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
