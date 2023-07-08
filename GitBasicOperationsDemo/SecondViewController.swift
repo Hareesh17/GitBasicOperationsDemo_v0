ea1e06dbe111c1e41f3119dc0e2e22e7e842288a
Hareesh Gangadhara <hareesh.b@tatadigital.com>
2023-07-08 12:23:25 +0530

    Added new line

---
+++ GitBasicOperationsDemo/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  GitBasicOperationsDemo
+//
+//  Created by Hareesh Gangadhara on 08/07/23.
+//  Copyright © 2023 Hareesh Gangadhara. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("SecondViewController")
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
